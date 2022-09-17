9926c5e1802ef92194bd5b51a5bc579a43c50fdc
Zak Farmer <zak@3sidedcube.com>
2022-09-17 22:43:51 +0100

    merge

--- src/constants.rs
+++ src/constants.rs
@@ -1,2 +1,3 @@
-pub const SCREEN_WIDTH: usize = 1280;
-pub const SCREEN_HEIGHT: usize = 720;
+pub const NUM_BODIES: usize = 300;
+pub const SCREEN_WIDTH: usize = 1920;
+pub const SCREEN_HEIGHT: usize = 1080;
